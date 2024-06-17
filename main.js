5749dd72da85d52577de91c814b21e61b701c691
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-06-17 10:46:46 +0100

    hovering button

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ document.querySelectorAll('.picture').forEach(div => {
   const button = div.querySelector('.shopbutton')
   //  button's class here
   // eslint-disable-next-line no-return-assign
-  div.addEventListener('mouseenter', () => button.style.backgroundColor = 'blue')
+  div.addEventListener('mouseenter', () => button.style.backgroundColor = ' #1a2456')
+  // eslint-disable-next-line no-return-assign
+  div.addEventListener('mouseenter', () => button.style.display = 'block')
   // eslint-disable-next-line no-return-assign
   div.addEventListener('mouseleave', () => button.style.backgroundColor = 'transparent')
+  // eslint-disable-next-line no-return-assign
+  div.addEventListener('mouseleave', () => button.style.display = 'none')
+})
+
+document.querySelectorAll('.picture').forEach(div => {
+  const price = div.querySelector('.price-tag')
+  // eslint-disable-next-line no-return-assign
+  div.addEventListener('mouseenter', () => price.style.display = 'none')
+  // eslint-disable-next-line no-return-assign
+  div.addEventListener('mouseleave', () => price.style.display = 'block')
 })
